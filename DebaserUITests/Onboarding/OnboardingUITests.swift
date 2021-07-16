ef0bf598c9da33241fdae2bed03650378c2fa622
Markus Bergh <hi@markusbergh.se>
2021-07-16 09:50:50 +0200

    Adding more UI tests (#5)
    * Added ui tests for more views

---
+++ DebaserUITests/Onboarding/OnboardingUITests.swift
@@ -0,0 +1,83 @@
+//
+//  OnboardingUITests.swift
+//  DebaserUITests
+//
+//  Created by Markus Bergh on 2021-07-15.
+//
+
+import XCTest
+
+class OnboardingUITests: XCTestCase {
+    
+    let app = XCUIApplication()
+    
+    private var skipButton: XCUIElement {
+        return OnboardingScreen.skipButton.element
+    }
+    
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        app.launchArguments = ["-resetUserDefaults", "-testOnboarding"]
+        app.launch()
+    }
+
+    override func tearDown() {
+        app.terminate()
+    }
+    
+    func testOnboardingDidShow() throws {
+        XCTAssert(OnboardingScreen.onboardingStep1Title.element.waitForExistence(timeout: 10.0))
+    }
+    
+    func testOnboardingHasExpectedPages() throws {
+        app.swipeLeft()
+        XCTAssert(OnboardingScreen.onboardingStep2Title.element.exists)
+        
+        app.swipeLeft()
+        XCTAssert(OnboardingScreen.onboardingStep3Title.element.exists)
+        
+        app.swipeLeft()
+        XCTAssert(OnboardingScreen.onboardingStep4Title.element.exists)
+        
+        // Final page should not have a skip button visible
+        XCTAssertTrue(!skipButton.isHittable && !skipButton.exists)
+    }
+    
+    func testOnboardingPushToSettingsView() throws {
+        app.swipeLeft()
+        app.swipeLeft()
+        app.swipeLeft()
+        
+        XCTAssert(OnboardingScreen.spotifyButton.element.isHittable)
+        OnboardingScreen.spotifyButton.element.tap()
+        
+        // User should end up in settings tab and pushed automatically to Spotify settings
+        XCTAssert(app.staticTexts["Spotify"].waitForExistence(timeout: 10.0))
+        XCTAssert(app.staticTexts["No active connection"].exists)
+    }
+        
+}
+
+enum OnboardingScreen: String {
+    case onboardingStep1Title = "Use Spotify to preview"
+    case onboardingStep2Title = "Share gigs with iMessage"
+    case onboardingStep3Title = "Find that upcoming gig"
+    case onboardingStep4Title = "Almost done! 🏄‍♀️ What is next?"
+    
+    case skipButton = "Skip"
+    
+    // Accessed by identifier
+    case spotifyButton = "OnboardingSpotifyButton"
+
+    var element: XCUIElement {
+        switch self {
+        case .onboardingStep1Title, .onboardingStep2Title, .onboardingStep3Title, .onboardingStep4Title:
+            return XCUIApplication().staticTexts.matching(identifier: self.rawValue).element
+        case .skipButton:
+            return XCUIApplication().buttons.matching(identifier: self.rawValue).element
+        case .spotifyButton:
+            return XCUIApplication().buttons.matching(identifier: self.rawValue).element
+        }
+    }
+}
--- DebaserUITests/Settings/SettingsUITests.swift
+++ DebaserUITests/Settings/SettingsUITests.swift
@@ -1,13 +1,13 @@
 //
-//  DebaserUITests.swift
+//  SettingsUITests.swift
 //  DebaserUITests
 //
-//  Created by Markus Bergh on 2021-05-06.
+//  Created by Markus Bergh on 2021-07-15.
 //
 
 import XCTest
 
-class DebaserUITests: XCTestCase {
+class SettingsUITests: XCTestCase {
     
     let app = XCUIApplication()
     
@@ -16,24 +16,18 @@ class DebaserUITests: XCTestCase {
         continueAfterFailure = false
         app.launchArguments = ["-resetUserDefaults", "-skipOnboarding"]
         app.launch()
+        
+        let tab = MainScreen.tabSettings.element
+        XCTAssert(tab.exists)
+        tab.tap()
     }
 
     override func tearDown() {
         super.tearDown()
         app.terminate()
     }
-
-    func testList() {
-        XCTAssert(app.staticTexts["Today's events"].exists)
-        XCTAssert(app.staticTexts["Stockholm"].exists)
-        XCTAssert(app.staticTexts["All events"].exists)
-    }
     
-    func testSettings() {
-        let tab = MainScreen.tabSettings.element
-        XCTAssert(tab.exists)
-        tab.tap()
-        
+    func testSettingsUserCanOverrideTheme() throws {
         XCTAssert(app.staticTexts["Settings"].waitForExistence(timeout: 10))
 
         XCTAssert(SettingsScreen.toggleThemeSystem.element.waitForExistence(timeout: 10))
@@ -45,37 +39,28 @@ class DebaserUITests: XCTestCase {
         XCTAssert(SettingsScreen.toggleThemeUser.element.waitForExistence(timeout: 10))
         XCTAssert(SettingsScreen.toggleThemeUser.element.isHittable)
     }
-}
-
-enum MainScreen: String {
-    case tabList = "TabItemList"
-    case tabFavourites = "TabItemFavourites"
-    case tabSettings = "TabItemSettings"
     
-    var element: XCUIElement {
-        switch self {
-        case .tabList, .tabFavourites, .tabSettings:
-            return XCUIApplication().buttons.matching(identifier: self.rawValue).element
-        }
+    func testSettingsUserCanOpenOnboarding() throws {
+        XCTAssert(app.staticTexts["Settings"].waitForExistence(timeout: 10))
+
+        XCTAssert(SettingsScreen.buttonOnboarding.element.exists)
+        SettingsScreen.buttonOnboarding.element.tap()
+        
+        XCTAssert(OnboardingScreen.onboardingStep1Title.element.waitForExistence(timeout: 10.0))
     }
 }
 
 enum SettingsScreen: String {
     case toggleThemeSystem = "ToggleSystemTheme"
     case toggleThemeUser = "ToggleUserTheme"
+    case buttonOnboarding = "Show onboarding"
     
     var element: XCUIElement {
         switch self {
         case .toggleThemeSystem, .toggleThemeUser:
             return XCUIApplication().switches.matching(identifier: self.rawValue).element
-        }
-    }
-}
-
-extension XCUIElement {
-    func tapUnhittable() {
-        XCTContext.runActivity(named: "Tap \(self) by coordinate") { _ in
-            coordinate(withNormalizedOffset: CGVector(dx: 0.0, dy: 0.0)).tap()
+        case .buttonOnboarding:
+            return XCUIApplication().buttons[self.rawValue]
         }
     }
 }
